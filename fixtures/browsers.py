e9189ef9ea30abdbf064801a6868b7a99bf38e72
s.lubimov <s.lubimov@fssoft.ru>
2026-01-26 02:26:57 +0300

    6.7 Параметризация Pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -14,6 +14,7 @@ def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
+
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
@@ -33,10 +34,8 @@ def initialize_browser_state(playwright: Playwright):
 
 
 @pytest.fixture
-@pytest.mark.usefixtures('initialize_browser_state')
-def chromium_page_with_state(playwright: Playwright) -> Page:
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
-    page = context.new_page()
-    yield page
+    yield context.new_page()
     browser.close()
