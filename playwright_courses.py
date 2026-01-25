920fb6877a98129c477308e72329b3bb29796070
s.lubimov <s.lubimov@fssoft.ru>
2026-01-25 18:43:26 +0300

    5.6 Работа с авторизацией в Playwright

--- playwright_courses.py
+++ playwright_courses.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
 
 with sync_playwright() as playwright:
@@ -27,4 +27,19 @@ with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
     page = context.new_page()
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+    header_courses = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(header_courses).to_be_visible()
+    expect(header_courses).to_have_text('Courses')
+
+    empty_tittle = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_tittle).to_be_visible()
+    expect(empty_tittle).to_have_text('There is no results')
+
+    empty_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_icon).to_be_visible()
+
+    empty_description = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_description).to_be_visible()
+    expect(empty_description).to_have_text('Results from the load test pipeline will be displayed here')
